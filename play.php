b1f2a2608fcc33048b73e090336190605041564b
Syed Huda <syed.huda@gmail.com>
2015-08-02 17:29:37 +0100

    Added properties.

--- play.php
+++ play.php
@@ -3,9 +3,29 @@
 class Ship
 {
   public $name;
+  public $weaponPower = 0;
+  public $jediFactor = 0;
+  public $strength = 0;
+
+  public function sayHello()
+  {
+    echo 'hello';
+  }
+
+  public function getName()
+  {
+    return $this->name;
+  }
 }
 
 $myShip = new Ship();
 $myShip->name = 'Jedi Starship';
+$myShip->weaponPower = 10;
 
 echo 'Ship name: ' . $myShip->name;
+echo '<hr />';
+$myShip->sayHello();
+echo '<hr />';
+echo $myShip->getName();
+echo '<hr />';
+var_dump($myShip->weaponPower);
