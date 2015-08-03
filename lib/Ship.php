90e9744a39df02818ec9a1702fc91321de7245e1
Syed Huda <syed.huda@gmail.com>
2015-08-03 16:08:11 +0100

    Created Constructor and functional ships.

--- lib/Ship.php
+++ lib/Ship.php
@@ -6,6 +6,18 @@ class Ship
   private $weaponPower = 0;
   private $jediFactor = 0;
   private $strength = 0;
+  private $underRepair;
+
+  public function __construct($name)
+  {
+    $this->name = $name;
+    $this->underRepair = mt_rand(1, 100) < 30;
+  }
+
+  public function isFunctional()
+  {
+    return !$this->underRepair;
+  }
 
   public function sayHello()
   {
