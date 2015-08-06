1c09f815398f7ee0e1302a1c90b5950ec4a39614
Syed Huda <syed.huda@gmail.com>
2015-08-06 20:42:28 +0100

    Added dummy db connection and data.

--- lib/ShipLoader.php
+++ lib/ShipLoader.php
@@ -4,32 +4,27 @@ class ShipLoader
 {
   public function getShips()
   {
+    $shipsData = $this->queryForShips();
     $ships = array();
+    foreach ($shipsData as $shipData)
+    {
+      $ship = new Ship($shipData['name']);
+      $ship->setWeaponPower($shipData['weapon_power']);
+      $ship->setJediFactor($shipData['jedi_factor']);
+      $ship->setStrength($shipData['strength']);
 
-    $ship = new Ship('Jedi Starfighter');
-    $ship->setWeaponPower(5);
-    $ship->setJediFactor(15);
-    $ship->setStrength(30);
-    $ships['starfighter'] = $ship;
-
-    $ship2 = new Ship('CloakShape Fighter');
-    $ship2->setWeaponPower(2);
-    $ship2->setJediFactor(2);
-    $ship2->setStrength(70);
-    $ships['cloakshape_fighter'] = $ship2;
-
-    $ship3 = new Ship('Super Star Destroyer');
-    $ship3->setWeaponPower(70);
-    $ship3->setJediFactor(0);
-    $ship3->setStrength(500);
-    $ships['super_star_destroyer'] = $ship3;
-
-    $ship4 = new Ship('RZ-1 A-wing interceptor');
-    $ship4->setWeaponPower(4);
-    $ship4->setJediFactor(4);
-    $ship4->setStrength(50);
-    $ships['rz1_a_wing_interceptor'] = $ship4;
-
+      $ships[] = $ship;
+    }
     return $ships;
+  }
+
+  private function queryForShips()
+  {
+    $pdo = new PDO('mysql:host=localhost;dbname=oo_battle', 'root', 'root');
+    $statement = $pdo->prepare('SELECT * FROM ship');
+    $statement->execute();
+    $shipsArray = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    return $shipsArray;
   }
 }
