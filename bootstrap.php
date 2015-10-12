06f90f1a78daf155c6e3cdbf5a81c2f92ffecdd3
Syed Huda <syed.huda@gmail.com>
2015-10-12 15:44:36 +0100

    Added RebelShip class.

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/lib/Model/Ship.php';
+require_once __DIR__ . '/lib/Model/RebelShip.php';
 require_once __DIR__ . '/lib/Service/BattleManager.php';
 require_once __DIR__ . '/lib/Service/ShipLoader.php';
 require_once __DIR__ . '/lib/Model/BattleResult.php';
