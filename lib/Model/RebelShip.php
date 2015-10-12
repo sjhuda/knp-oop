c93e41758a7abf8aa03d3bd7f6a4b65781bc021a
Syed Huda <syed.huda@gmail.com>
2015-10-12 16:34:30 +0100

    Used parent function in RebelShip class.

--- lib/Model/RebelShip.php
+++ lib/Model/RebelShip.php
@@ -14,22 +14,9 @@ class RebelShip extends Ship
 
   public function getNameAndSpecs($useShortFormat = false)
   {
-    if ($useShortFormat) {
-      return sprintf(
-        '%s: w:%s, j:%s, s:%s (Rebel)',
-        $this->getName(),
-        $this->getWeaponPower(),
-        $this->getJediFactor(),
-        $this->getStrength()
-      );
-    } else {
-      return sprintf(
-        '%s: Weapon Power: %s, Jedi Factor: %s, Strength: %s (Rebel)',
-        $this->getName(),
-        $this->getWeaponPower(),
-        $this->getJediFactor(),
-        $this->getStrength()
-      );
-    }
+    $val = parent::getNameAndSpecs($useShortFormat);
+    $val .=  ' (Rebel)';
+
+    return $val;
   }
 }
