7739dd1a4b584c805b90d478964298be3abff1e8
Syed Huda <syed.huda@gmail.com>
2016-05-17 22:14:28 +0100

    RebelShip tests.

--- src/Test/Model/RebelShipTest.php
+++ src/Test/Model/RebelShipTest.php
@@ -15,4 +15,29 @@ class RebelShipTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals('Rebel', $type);
     }
+
+    /** @test */
+    public function it_is_always_functional()
+    {
+        $ship = new RebelShip('Rebel Ship');
+        $this->assertTrue($ship->isFunctional());
+    }
+
+    /** @test */
+    public function it_has_a_jedi_factor_of_more_than_10()
+    {
+        $ship = new RebelShip('Rebel Ship');
+        $jediFactor = $ship->getJediFactor();
+
+        $this->assertGreaterThanOrEqual(10, $jediFactor);
+    }
+
+    /** @test */
+    public function it_has_a_jedi_factor_of_less_than_30()
+    {
+        $ship = new RebelShip('Rebel Ship');
+        $jediFactor = $ship->getJediFactor();
+
+        $this->assertLessThanOrEqual(30, $jediFactor);
+    }
 }
